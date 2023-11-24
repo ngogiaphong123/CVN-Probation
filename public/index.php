1ba8aa4d3c19d03f33e95451f1b6ecced56ecd15
ngogiaphong123 <ngogiaphong123@gmail.com>
2023-11-24 17:09:06 +0700

    docker

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 define('BASE_DIR', str_replace('/public', '', __DIR__));
-require __DIR__. '/../vendor/autoload.php';
-require __DIR__. '/../core/helpers.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
+
 $dotenv = Dotenv::createImmutable(BASE_DIR);
 $dotenv->load();
 
